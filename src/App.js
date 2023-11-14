b07d53e267d03305dd2f827de281f8b69a128432
Kassid <lnamkyung@gmail.com>
2023-11-14 22:15:57 +0900

    dnd prototype Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import EditPage from './pages/EditPage.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<EditPage/>}/>
+      </Routes>
     </div>
   );
 }
